08191401948a4ebe1436251d0871d3428e41b7f0
felipsbreno <brendo.filipe2050@gmail.com>
2023-06-16 16:27:12 -0300

    create the type from theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import { App } from "./App.tsx";
+import { defaultTheme } from "./styles/themes/default.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
